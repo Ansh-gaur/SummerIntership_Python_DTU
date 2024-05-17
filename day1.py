2725794f545f937fb92d8b601fd076b57b079dcb
Ansh-gaur <147902097+Ansh-gaur@users.noreply.github.com>
2024-05-17 19:36:37 +0530

    AnshGaurDay1.py

--- day1.py
+++ day1.py
@@ -14,8 +14,7 @@ for i in range(n):
     d["Quantity"]=b
     L.append(d) 
 
-#r=p.DataFrame(L)
-#print(r)
+
 def Find_QTY(ide,L):
     for i in L:
         if i["ID"]==ide:
@@ -36,27 +35,3 @@ for i in range(2,size+2):
 A.save(path)
 
 
-'''
-t=p.read_excel(path,sheet_name='Sheet1',usecols=("ID","Quantity"))
-print(t)
-for r in dataframe_to_rows(t):
-    if(len(r)==3):
-        print("ro:",r[0])
-        if( r[0]!=None):
-            r[2]=Find_QTY(r[1],L)
-            print(r[2])
-            print(r)
-print(t)
- '''       
-        
-        
-'''
-A=load_workbook(path)
-B=A["Sheet1"]
-for i in dataframe_to_rows(,index=False):
-    print(i)
-    B.append(i)
-A.save(path)
-
-'''
-   
