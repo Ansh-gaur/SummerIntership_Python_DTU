3cdd0d96a9644a47ced2fa001f35750ae158ac67
Ansh-gaur <147902097+Ansh-gaur@users.noreply.github.com>
2024-05-18 12:52:49 +0530

    WritingXL_day1_AnshGaur.py

--- WritingXL_day1_AnshGaur.py
+++ WritingXL_day1_AnshGaur.py
@@ -14,17 +14,21 @@ def Find_QTY(ide,L):
             return i["Quantity"]
 
     
-path="C:\\Users\\hp\\Desktop\\PythonDTU\\day1.xlsx"     #You can write the path of your file
+path="C:\\Users\\hp\\Desktop\\PythonDTU\\sample sheet.xlsx"     #You can write the path of your file
 A=load_workbook(path)
-B=A['Sheet1']
-t=p.read_excel(path,sheet_name='Sheet1',usecols=("Quantity","ID"))
+B=A['Sheet 1']
+t=p.read_excel(path,sheet_name='Sheet 1')
 size=len(t)
-for i in range(3,size+3):
+
+for i in range(3,132):
     y='A'+str(i)
     rr='B'+str(i)
-    B[y]=Find_QTY(B[rr].value,outputFinal)
+    ttt=Find_QTY(B[rr].value,outputFinal)
+    if(ttt!=None):
+        B[y]=ttt
+    else:
+        B[y]=B[y].value
 A.save(path)
-
 
 
    
