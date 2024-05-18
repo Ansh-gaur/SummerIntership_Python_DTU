68ab86450b43d98444c7c8acff2c1a4a86a5c90f
ChinmayeMankt <mankataliachinmaye@gmail.com>
2024-05-18 13:32:52 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -2,18 +2,18 @@ import openpyxl
 import pandas as pd
 
 mylist = [{'ID':'2', 'Quantity':0},{'ID':'2.1','Quantity':0},{'ID':'2.1.1','Quantity':0},{'ID':'2.1.2','Quantity':0},{'ID':'2.1.3','Quantity':94.10902061862528}]
-file = 'C://Users//LENOVO//Desktop//data.xlsx'
-data = pd.read_excel(file, sheet_name='Sheet1')
+file = 'C://Users//LENOVO//Desktop//sample sheet.xlsx'
+data = pd.read_excel(file, sheet_name='Sheet 1')
+print(data['Unnamed: 1'])
 workbook = openpyxl.load_workbook(file)
-sheet = workbook['Sheet1']
-rows = 5
+sheet = workbook['Sheet 1']
+
 for dictionary in mylist:
-    for i in range(0,5):
-        if str(data['ID'][i]) == str(dictionary['ID']):
-            sheet.cell(row = i+2, column=2).value = dictionary['Quantity']
+    for i in range(1,129):
+        if str(data['Unnamed: 1'][i]) == str(dictionary['ID']):
+            sheet.cell(row = i+2, column=1).value = dictionary['Quantity']
 
 workbook.save(file)
-            
 
 
 
