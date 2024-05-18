c7b87c74acb7c9e3c5b19611a6d41fb979d66062
ChinmayeMankt <mankataliachinmaye@gmail.com>
2024-05-18 14:09:29 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -8,7 +8,7 @@ workbook = openpyxl.load_workbook(file)
 sheet = workbook['Sheet 1']
 
 for dictionary in mylist:
-    for i in range(1,129):
+    for i in range(1,130):
         if str(data['Unnamed: 1'][i]) == str(dictionary['ID']):
             sheet.cell(row = i+2, column=1).value = dictionary['Quantity']
             break
