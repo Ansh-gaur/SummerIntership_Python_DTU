a758507d912053340ca170238980a75ebbb53a32
ChinmayeMankt <mankataliachinmaye@gmail.com>
2024-05-18 13:38:26 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -4,7 +4,6 @@ import pandas as pd
 mylist = [{'ID':'2', 'Quantity':0},{'ID':'2.1','Quantity':0},{'ID':'2.1.1','Quantity':0},{'ID':'2.1.2','Quantity':0},{'ID':'2.1.3','Quantity':94.10902061862528}]
 file = 'C://Users//LENOVO//Desktop//sample sheet.xlsx'
 data = pd.read_excel(file, sheet_name='Sheet 1')
-print(data['Unnamed: 1'])
 workbook = openpyxl.load_workbook(file)
 sheet = workbook['Sheet 1']
 
