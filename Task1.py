625a4de895af3c023ccbd79cd6f943436ff28da2
ChinmayeMankt <mankataliachinmaye@gmail.com>
2024-05-22 16:59:01 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -8,13 +8,12 @@ workbook = openpyxl.load_workbook(file)
 sheet = workbook['Sheet 1']
 
 for dictionary in mylist:
-    for i in range(1,140):
-        try:
-            if str(data['Unnamed: 1'][i]) == str(dictionary['ID']):
-                sheet.cell(row = i+2, column=1).value = dictionary['Quantity']
-                break
-        except Exception as e:
+    i = 1
+    while data['Unnamed: 1'][i]:
+        if str(data['Unnamed: 1'][i]) == str(dictionary['ID']):
+            sheet.cell(row = i+2, column=1).value = dictionary['Quantity']
             break
+        i+=1
 workbook.save(file)
 
 
